fe322f8c51439955abaeda481f509024f9d7f214
Valério Hasman <valeriopro22@gmail.com>
2023-08-21 16:31:40 -0300

    teste2

--- teste.js
+++ teste.js
@@ -1,15 +1,15 @@
 (() => {
   'use strict'
 
-  let exibir = true;
-  const elementos = paratela();
+  let printar = true;
+  const elementos = preElem();
 
   window.addEventListener('error', erros);
 
   function erros(e){
-    if(exibir){
-      exibe();
-      exibir = false;
+    if(printar){
+      printe();
+      printar = false;
     }
 
     const err = {
@@ -22,43 +22,20 @@
     elementos.pre.innerHTML += JSON.stringify(err, 0, 2) + "\n\n";
   }
 
-  function exibe(){
+  function printe(){
     window.addEventListener('load', ()=>{
-      document.body.insertAdjacentElement('beforeend', elementos.button);
-      document.body.insertAdjacentElement('beforeend', elementos.div);
+      document.body.insertAdjacentElement('beforeend', elementos.pre);
     })
   }
 
-  function paratela(){
+  function preElem(){
     const div = document.createElement('div');
 
-    div.innerHTML = `<!-- Button trigger modal -->
-    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Launch bug demo modal
-    </button>
-    
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-fullscreen">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Erros:</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <pre></pre>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>`;
+    div.innerHTML = `
+    <pre class="bg-body p-5 m-2 rounded-5"></pre>
+    `;
 
     return {
-      button: div.querySelector('button'),
-      div: div.querySelector('.modal'),
       pre: div.querySelector('pre')
     }
   }
